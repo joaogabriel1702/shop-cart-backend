e2e935989e21507655dd7a72b145f38eacf18fee
joaogabriel1702 <joaogabrielcsoares@hotmail.com>
2025-08-05 15:06:03 -0300

    display cart

--- src/services/cart.js
+++ src/services/cart.js
@@ -14,13 +14,22 @@ async function removeItem(userCart, index) {
 }
 
 async function calculateTotal(userCart) {
+    console.log("\nShopee Cart TOTAL IS:")
     const result = userCart.reduce((total, item)=>total + item.subtotal(), 0)
-    console.log(result)
+    console.log(`Total: ${result}`)
+}
+
+async function displayCart(userCart) {
+    console.log("Shopee cart list:")
+    userCart.forEach((item, index) => {
+        console.log(`${index + 1}. ${item.name} - R$ ${item.price} | ${item.quantity}x | Subtotal: ${item.subtotal()}`)
+    });
 }
 
 export {
     addItem,
     calculateTotal,
     deleteItem,
-    removeItem
+    removeItem,
+    displayCart
 }
